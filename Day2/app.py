daf8908e16bb766a72aa08241b60546f6bbe7d9f
hrmiitm <hrithik999999@gmail.com>
2026-03-19 10:21:45 +0530

    xyz --> app

--- Day2/app.py
+++ Day2/app.py
@@ -1,11 +1,11 @@
 from flask import Flask # Class
 
-xyz = Flask(__name__) # Object
+app = Flask(__name__) # Object
 
-@xyz.route('/') # Registring the function with url
+@app.route('/') # Registring the function with url
 def home():
     x = 10
     return f"Hello From Home Function {x}"
     
 
-xyz.run() # Object Method ---> whose object ---> of Flask Class
+app.run() # Object Method ---> whose object ---> of Flask Class
